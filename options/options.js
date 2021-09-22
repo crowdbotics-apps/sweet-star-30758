4060c4480e3928bf7f26df45ce8541b5efed77b2
Crowdbotics <team@crowdbotics.com>
2021-09-22 21:37:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sweet_star_30758Identifier",
+  url: "https://sweet_star_30758Identifier.botics.co",
+  api: "https://sweet_star_30758Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
